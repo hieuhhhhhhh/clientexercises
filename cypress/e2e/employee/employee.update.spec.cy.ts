7ce70be9415c811c240f0190664fce5a21947840
hieuhhhhhhh <hieuhaihuoc31102@gmail.com>
2024-09-23 21:59:20 -0400

    lab4 done

---
+++ cypress/e2e/employee/employee.update.spec.cy.ts
@@ -0,0 +1,12 @@
+describe('employee update test', () => {
+  it('visits the employee page and updates an employee', () => {
+    cy.visit('/');
+    cy.get('button').click();
+    cy.contains('a', 'employees').click();
+    cy.contains('Nhat').click(); // replace Slick with your own name
+    cy.get("[type='email']").clear();
+    cy.get("[type='email']").type('someemail@domain.com');
+    cy.get('form').submit();
+    cy.contains('updated!');
+  });
+});
