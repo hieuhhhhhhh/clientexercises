ecc0c938fc5c93e684e972b6ea465429d6b1464d
hieuhhhhhhh <hieuhaihuoc31102@gmail.com>
2024-10-02 16:18:18 -0400

    lab7 continue

--- src/app/expense/expense-home/expense-home.component.ts
+++ src/app/expense/expense-home/expense-home.component.ts
@@ -4,6 +4,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatComponentsModule } from '@app/mat-components/mat-components.module';
 import { Expense } from '@app/expense/expense';
 import { ExpenseService } from '@app/expense/expense.service';
+import { Sort } from '@angular/material/sort';
+
 @Component({
   selector: 'app-expense-home',
   standalone: true,
@@ -25,5 +27,34 @@ export class ExpenseHomeComponent implements OnInit {
         (this.msg = `Failed to load expenses - ${e.message}`),
       complete: () => (verbose ? (this.msg = `Expenses loaded!`) : null),
     });
+  }
+
+  sortExpensesWithObjectLiterals(sort: Sort): void {
+    const literals = {
+      id: () =>
+        (this.dataSource.data = this.dataSource.data.sort(
+          (a: Expense, b: Expense) =>
+            sort.direction === 'asc' ? a.id - b.id : b.id - a.id,
+        )),
+      employeeid: () =>
+        (this.dataSource.data = this.dataSource.data.sort(
+          (a: Expense, b: Expense) =>
+            sort.direction === 'asc'
+              ? a.employeeid - b.employeeid
+              : b.employeeid - a.employeeid,
+        )),
+      dateincurred: () =>
+        (this.dataSource.data = this.dataSource.data.sort(
+          (a: Expense, b: Expense) =>
+            sort.direction === 'asc'
+              ? a.dateincurred < b.dateincurred
+                ? -1
+                : 1
+              : b.dateincurred < a.dateincurred
+                ? -1
+                : 1,
+        )),
+    };
+    literals[sort.active as keyof typeof literals]();
   }
 }
